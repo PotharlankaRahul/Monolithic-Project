621ba479416e2fcc02d49cb04101706b0d754302
PotharlankaRahul <121004749+PotharlankaRahul@users.noreply.github.com>
2025-12-28 15:24:10 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik1122.monobucket"
+  bucket = "rahul.projects.flm "
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -16,16 +16,3 @@ resource "aws_s3_bucket_acl" "three" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "three" {
-bucket = aws_s3_bucket.one.id
-versioning_configuration {
-status = "Enabled"
-}
-}
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafashaik1122.monobucket"
-key = "prod/terraform.tfstate"
-}
-}
